7cb72fd4a7e79ef4b0eacd5b1da259ac9d33e0f9
Nikita Filonenko <nikdfil@yandex.ru>
2024-01-16 23:17:32 +0300

    Update project v. 1

--- app/src/main/java/com/example/photogalleryapp/CosmoFocusApp.kt
+++ app/src/main/java/com/example/photogalleryapp/CosmoFocusApp.kt
@@ -1,14 +1,15 @@
 package com.example.photogalleryapp
 
 import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
 import coil.ImageLoader
 import coil.ImageLoaderFactory
 import coil.decode.VideoFrameDecoder
 
-class CosmoFocusApp : Application(), ImageLoaderFactory {
-    override fun newImageLoader(): ImageLoader = ImageLoader.Builder(this)
-        .components {
-            add(VideoFrameDecoder.Factory())
-        }
-        .build()
+class CosmoFocusApp : Application(), CameraXConfig.Provider {
+    override fun getCameraXConfig(): CameraXConfig {
+        return CameraXConfig.Builder.fromConfig(Camera2Config.defaultConfig())
+            .build()
+    }
 }
