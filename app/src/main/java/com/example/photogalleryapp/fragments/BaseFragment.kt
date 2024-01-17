263f1923e33aaf20f84d78bcfa2aa56f5727644b
Nikita Filonenko <nikdfil@yandex.ru>
2024-01-17 19:30:48 +0300

    Update project v. 6

--- app/src/main/java/com/example/photogalleryapp/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/photogalleryapp/fragments/BaseFragment.kt
@@ -4,50 +4,28 @@ import android.Manifest
 import android.content.ContentUris
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import com.example.photogalleryapp.R
 import com.example.photogalleryapp.adapter.Media
 import com.google.android.material.snackbar.Snackbar
-import java.io.File
+
 
 
 abstract class BaseFragment : Fragment() {
-    /**
-     *  Generic ViewBinding of the subclasses
-     * */
+
     abstract val binding: ViewBinding
-
-    // The Folder location where all the files will be stored
-    protected val outputDirectory: String by lazy {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            "${Environment.DIRECTORY_DCIM}/CosmoFocus/"
-        } else {
-            "${requireContext().getExternalFilesDir(Environment.DIRECTORY_DCIM)}/CosmoFocus/"
-        }
-    }
-
-    // The permissions we need for the app to work properly
-    private val permissions = mutableListOf(
-        Manifest.permission.CAMERA,
-        Manifest.permission.RECORD_AUDIO
-    ).apply {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            add(Manifest.permission.ACCESS_MEDIA_LOCATION)
-        }
-    }
+    protected val outputDirectory: String = "${Environment.DIRECTORY_DCIM}/CosmoFocus/"
+    private val permissions = mutableListOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
 
     private val permissionRequest = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
         if (permissions.all { it.value }) {
@@ -64,16 +42,16 @@ abstract class BaseFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        // Adding an option to handle the back press in fragment
-        requireActivity().onBackPressedDispatcher.addCallback(
-            viewLifecycleOwner,
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    onBackPressed()
-                }
-            })
-
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (allPermissionsGranted()) {
+            onPermissionGranted()
+        } else {
+            permissionRequest.launch(permissions.toTypedArray())
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -89,29 +67,19 @@ abstract class BaseFragment : Fragment() {
 
 
     private fun getMediaQPlus(): List<Media> {
-        val videoItems = queryMedia(
-            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-            arrayOf(
-                MediaStore.Video.Media._ID,
-                MediaStore.Video.Media.RELATIVE_PATH,
-                MediaStore.Video.Media.DATE_TAKEN,
-            ),
-            null,
-            null,
-            "${MediaStore.Video.Media.DISPLAY_NAME} ASC"
+        val videoProjection = arrayOf(
+            MediaStore.Video.Media._ID,
+            MediaStore.Video.Media.RELATIVE_PATH,
+            MediaStore.Video.Media.DATE_TAKEN
+        )
+        val imageProjection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.RELATIVE_PATH,
+            MediaStore.Images.Media.DATE_TAKEN
         )
 
-        val imageItems = queryMedia(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            arrayOf(
-                MediaStore.Images.Media._ID,
-                MediaStore.Images.Media.RELATIVE_PATH,
-                MediaStore.Images.Media.DATE_TAKEN,
-            ),
-            null,
-            null,
-            "${MediaStore.Images.Media.DISPLAY_NAME} ASC"
-        )
+        val videoItems = queryMedia(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, videoProjection, null, null, "${MediaStore.Video.Media.DISPLAY_NAME} ASC")
+        val imageItems = queryMedia(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, imageProjection, null, null, "${MediaStore.Images.Media.DISPLAY_NAME} ASC")
 
         return videoItems + imageItems
     }
@@ -121,13 +89,7 @@ abstract class BaseFragment : Fragment() {
         val items = mutableListOf<Media>()
         val contentResolver = requireContext().applicationContext.contentResolver
 
-        contentResolver.query(
-            uri,
-            projection,
-            selection,
-            selectionArgs,
-            sortOrder
-        )?.use { cursor ->
+        contentResolver.query(uri, projection, selection, selectionArgs, sortOrder)?.use { cursor ->
             val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
             val pathColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.RELATIVE_PATH)
             val dateColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATE_TAKEN)
@@ -144,23 +106,15 @@ abstract class BaseFragment : Fragment() {
                 }
             }
         }
+
         return items
     }
 
-    /**
-     * Check for the permissions
-     */
     protected fun allPermissionsGranted() = permissions.all {
         ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
     }
 
-    /**
-     * A function which will be called after the permission check
-     * */
     open fun onPermissionGranted() = Unit
 
-    /**
-     * An abstract function which will be called on the Back button press
-     * */
     abstract fun onBackPressed()
 }
